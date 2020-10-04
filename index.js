78e8922ea250020351d859342d738ec62fb31bee
KouKiriyama03 <josuepanky@gmail.com>
2020-10-03 20:27:01 -0600

    inicio del calculo del metodo frances

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const express = require("express");
 // Importar handlebars como template engine
  const exphbs = require("express-handlebars");
 // Importar body parser que nos permite acceder al cuerpo de la petición HTTP
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser"); 
+
 // Importar la función de cálculo de método francés
-// const { calcularMetodoFrances } = require("./calculoMetodoFrances");
+const { calcularMetodoFrances } = require("./calculoMetodoFrances");
 
 // Crear un servidor express
 const app = express();
@@ -17,18 +18,16 @@ app.set("view engine", "hbs");
 
 
 //  Habilitar body parse para leer los datos del cuerpo de peticiones POST
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Crear una ruta para /
-
+// Crear una ruta para 
 // Información sobre los verbos HTTP
 // https://developer.mozilla.org/es/docs/Web/HTTP/Methods
 app.get("/", (req, res, next) => {
  res.render("formulario_prestamo");
-});
-
-
-/*app.post("/prestamo", (req, res, next) => {
+}); 
+app.post("/prestamo", (req, res, next) => {
+  console.log(req.body);
   // Asignación por destructuring
   // https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Operadores/Destructuring_assignment
   const { monto, tasaInteres, periodo } = req.body;
@@ -36,9 +35,9 @@ app.get("/", (req, res, next) => {
   calcularMetodoFrances(monto, tasaInteres, periodo);
 
   res.send("Petición aceptada");
-});*/
+});
 
 // Inicializar el servidor en un puerto en específico
 app.listen(5005, () => {
-  console.log("Servidor ejecutándose en el puerto 5005");
+  console.log('Servidor ejecutándose en el puerto 5005');
 });
